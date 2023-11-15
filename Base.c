fc3dd4b091eb81c7181811230c738a6bd23d97b0
Daniel Henao <102403567+Danny552@users.noreply.github.com>
2023-11-14 22:50:20 -0500

    arreglado el base.c

--- Base.c
+++ Base.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <conio.h>
+#include <windows.h>
 
 #define MAX_Votantes 150
 
+void MenuAdmin();
+void MenuConsultas();
+
 struct Votante
 {
-    char código[12]; //Documento identidad
+    char codigo[12]; //Documento identidad
     char nombre[60]; //Nombre
-    char contraseña[100]; //Contraseña establecida por el programa
+    char contra[100]; //Contraseña establecida por el programa
     bool voto; //Si ya votó o no
     int tipo; //Tipo de votante (1.Estudiante, 2.Egresado, 3.Docentes, 4.Administrativos)
 };
@@ -20,7 +25,7 @@ bool ConfirmarCedula(int contador, char cd[10]) //Función auxiliar que sirve pa
     int i;
     for(i=0;i<contador;i++)
     {
-        if(Votantes[i].código == cd)
+        if(Votantes[i].codigo == cd)
         {
             printf("Usuario ya registrado.\n");
             return false;
@@ -31,9 +36,9 @@ bool ConfirmarCedula(int contador, char cd[10]) //Función auxiliar que sirve pa
 
 struct Admin
 {
-    char código[12]; //Documento identidad
+    char codigo[12]; //Documento identidad
     char nombre[60]; //Nombre
-    char contraseña[100]; //Contraseña establecida
+    char contra[100]; //Contraseña establecida
 };
 
 struct Votos
@@ -48,29 +53,24 @@ struct Votos
 struct Candidato
 {
     char nombre[60]; //Nombre
-    char código[12]; //Documento identidad
-    char nTarjetón; //Número en el tarjetón
+    char codigo[12]; //Documento identidad
+    char nTar; //Número en el tarjetón
     struct Votos votos; //Cantidad de votos a favor
 };
 
 void crearArchivo()
 {
-    //Se crea un archivo donde se almacenarán los códigos
+    //Se crea un archivo donde se almacenarán los codigos
 }
 
-void registrarVotante()
+void RegistrarVotante()
 {
     //Opción para el administrativo para registrar un votante
 }
 
-void ElimiarVotante()
+void EliminarVotante()
 {
     //Opción del Admin para eliminar un votante
-}
-
-void EditarVotante()
-{
-    //Opción del Admin para editar un votante
 }
 
 void ConsultarVotantes()
@@ -78,7 +78,7 @@ void ConsultarVotantes()
     //Opción del administrador para consultar los datos de los votantes
 }
 
-void AñadirCandidato()
+void AnadirCandidato()
 {
     //Opción del administrador para registrar un candidato
 }
@@ -103,16 +103,87 @@ void ConsultarVotos()
     //Opción del administrador para consultar como van las votaciones
 }
 
-int menuVotante(){
+void votoNeto(){
+    //para ver el la cantidad total 
+}
+
+void VotoNetoTipo(){
+    //La cantidad total por tipo de votante
+}
+
+void VotoPonderadoTipo(){
+    //los votos después de hacer la ponderación
+}
+
+void Histograma(){
+    //Histograma
+}
+
+void IngresarAdministrador(){
+    char usuario[100];
+    int contra;
+    printf("Ingrese su usuario:");
+    gets(usuario);
+    printf("Ingrese su contra: ");
+    scanf("%d", &contra);
+    MenuAdmin(usuario);
+}
+void Menu(){
+    int opc;
+    printf ("1) Votante\n2) Administrador");
+    scanf ("%d", &opc);
+    switch(opc){
+        case 1:
+        RegistrarVotante();
+        break;
+        case 2:
+        IngresarAdministrador();
+        break;
+        }
+    }
+
+void MenuAdmin(char usuario[100]){
+    printf("BIENVENIDO %d\n", usuario[100]);
+    printf("Ingrese lo que desea realizar:\n");
+    int opc;
+    printf("1) Registrar votante\n2)Eliminar datos de votante\n3)Registrar candidato\n4)Eliminar candidato\n5)Finalizar y ver consultas (ESTO CIERRA LOS VOTOS)\n6)Salir.");
+    scanf("%d", &opc);
+    switch (opc)
+    {
+    case 1:
+    RegistrarVotante();
+        break;
+    case 2:
+    EliminarVotante();
+        break;
+    case 3:
+    AnadirCandidato();
+        break;
+    case 5:
+    MenuConsultas();
+        break;
+    case 6: 
+    Menu();
+        break;
+    default:
+    printf ("opción no válida");
+    Sleep(100);
+    system("Clear");
+    MenuAdmin(usuario);
+        break;
+    }
+}
+
+void MenuVotante(){
     int id;
     printf ("Ingrese su número de identificación: ");
     scanf ("%d", &id);
-    printf ("Ingrese su contraseña (Dada por el administrador):");
+    printf ("Ingrese su contra (Dada por el administrador):");
     scanf ("%d", &id);
     printf ("Ingrese su candidato a votar:");
     printf ("1)-----------\n2)-----------\n3)-----------\n4)-----------\n5)-----------\n6)-----------");
 }
-int menuConsultas(){
+void MenuConsultas(){
     do{
     printf("Ingrese lo que desee ver: ");
     printf("1)Votos netos\n2)Porcentaje neto de votos\n2)Porcentaje de votos ponderados\n3)Histograma de frecuencia de votos");
@@ -124,13 +195,13 @@ int menuConsultas(){
     votoNeto();
     break;
         case 2:
-    votoNetoPor();
+    VotoNetoTipo();
     break;
         case 3:
-    votoPonderadoPor();
+    VotoPonderadoTipo();
     break;
         case 4:
-    histograma();
+    Histograma();
     break;
         case 5:
         break;
@@ -140,58 +211,7 @@ int menuConsultas(){
     }
     } while (0);
 }
-int menuAdmin(char usuario[100]){
-    printf("BIENVENIDO %d\n", cupper(usuario[100]));
-    printf("Ingrese lo que desea realizar:\n");
-    int opc;
-    printf("1) Registrar votante\n2)Eliminar datos de votante\n3)Modificar datos de votante\n4)Registrar candidato\n5)Eliminar candidato\n6)Finalizar y ver consultas (ESTO CIERRA LOS VOTOS)\n6)\n7)Salir.");
-    scanf("%d", &opc);
-    switch (opc)
-    {
-    case 1:
-    registrarVotante();
-        break;
-    case 2:
-    eliminarVotante();
-    case 3:
-    modificarVotante();
-    break;
-    case 4:
-    registrarCandidato();
-    case 5:
-    menuConsultas();
-    case 6: 
-    menu();
-    default:
-    printf ("opción no válida");
-    sleep(100);
-    system("Clear");
-    menuAdmin(usuario);
-        break;
-    }
+
+int main(){
+    Menu();
 }
-
-int ingresarAdministrador(){
-    char usuario[100];
-    int contra;
-    printf("Ingrese su usuario:");
-    scanf("%d", &usuario);
-    printf("Ingrese su contraseña: ");
-    scanf("%d", &contra);
-    menuAdmin(usuario);
-}
-
-int menu(){
-    int opc;
-    scanf ("%d", &opc);
-    switch(opc){
-        printf ("1) Votante\n2) Administrador");
-        case 1:
-        registrarVotante();
-        break;
-        case 2:
-        ingresarAdministrador();
-        break;
-        }
-    }
-
